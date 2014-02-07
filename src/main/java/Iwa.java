5af03ae47976aa7e97fe398db6070f329edf788e
oi <oi@mail.webfront.dev>
2014-02-08 01:41:37 +0900

    Iwa

---
+++ src/main/java/Iwa.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)$Id$
+ * 
+ * Copyright (c) 2010-2014 ISI corp. and others., All rights reserved.
+ */
+
+/**
+ * (ƒNƒ‰ƒXà–¾)
+ *
+ * @author  iwao_oi 2014/02/08 ‰”Åì¬
+ * @version 1.0
+ * @since   JDK 1.6.0
+ */
+public class Iwa
+{
+
+}
